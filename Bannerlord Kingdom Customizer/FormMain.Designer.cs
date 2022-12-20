c2e0151f3a0e3fcd67584ac2e1d605576963bfb7
Pat313 <bpatrik313@gmail.com>
2022-12-20 20:39:34 +0100

    Setup done

--- Bannerlord Kingdom Customizer/FormMain.Designer.cs
+++ Bannerlord Kingdom Customizer/FormMain.Designer.cs
@@ -28,19 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btnMinMax = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.pnlHeader.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.pnlHeader.Controls.Add(this.btnMinMax);
+            this.pnlHeader.Controls.Add(this.btnRestore);
+            this.pnlHeader.Controls.Add(this.btnCancel);
+            this.pnlHeader.Controls.Add(this.MainMenu);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1280, 30);
+            this.pnlHeader.TabIndex = 0;
+            this.pnlHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlHeader_MouseDown);
+            // 
+            // btnMinMax
+            // 
+            this.btnMinMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinMax.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinMax.BackgroundImage = global::Bannerlord_Kingdom_Customizer.Properties.Resources.tray;
+            this.btnMinMax.FlatAppearance.BorderSize = 0;
+            this.btnMinMax.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.btnMinMax.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.btnMinMax.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinMax.Location = new System.Drawing.Point(1152, 1);
+            this.btnMinMax.Name = "btnMinMax";
+            this.btnMinMax.Size = new System.Drawing.Size(42, 28);
+            this.btnMinMax.TabIndex = 2;
+            this.btnMinMax.UseVisualStyleBackColor = false;
+            this.btnMinMax.Click += new System.EventHandler(this.btnMinMax_Click);
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRestore.BackColor = System.Drawing.Color.Transparent;
+            this.btnRestore.BackgroundImage = global::Bannerlord_Kingdom_Customizer.Properties.Resources.minimize;
+            this.btnRestore.FlatAppearance.BorderSize = 0;
+            this.btnRestore.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.btnRestore.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.btnRestore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestore.Location = new System.Drawing.Point(1195, 1);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(42, 28);
+            this.btnRestore.TabIndex = 2;
+            this.btnRestore.UseVisualStyleBackColor = false;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.BackgroundImage = global::Bannerlord_Kingdom_Customizer.Properties.Resources.cancel;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Location = new System.Drawing.Point(1238, 1);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(42, 28);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.BackColor = System.Drawing.Color.Transparent;
+            this.MainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(1280, 28);
+            this.MainMenu.TabIndex = 3;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.pnlHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.MainMenu;
             this.Name = "FormMain";
-            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel pnlHeader;
+        private Button btnMinMax;
+        private Button btnRestore;
+        private Button btnCancel;
+        private MenuStrip MainMenu;
     }
 }
--- Bannerlord Kingdom Customizer/FormMain.cs
+++ Bannerlord Kingdom Customizer/FormMain.cs
@@ -1,19 +1,130 @@
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
-using MaterialSkin;
-using MaterialSkin.Controls;
 
 namespace Bannerlord_Kingdom_Customizer
 {
-    public partial class FormMain : MaterialForm
+    public partial class FormMain : Form
     {
-        private MaterialSkinManager themeManager = MaterialSkinManager.Instance;
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
 
         public FormMain()
         {
             InitializeComponent();
+        }
 
-            themeManager.Theme = MaterialSkinManager.Themes.DARK;
-            themeManager.ColorScheme = new ColorScheme(Primary.Green700, Primary.Green900, Primary.Green500, Accent.Green400, TextShade.WHITE);
+        private async void FormMain_Load(object sender, EventArgs e)
+        {
+            await Setup();
+        }
+
+        private async Task Setup()
+        {
+            await Task.Run(() =>
+            {
+                Application.EnableVisualStyles();
+
+                MainMenu.Renderer = new ToolStripProfessionalRenderer(new MenuColorTable());
+
+                List<string[]> items = new List<string[]> { new string[] { "New", "Open" }, new string[] { "New", "Open" } };
+                ToolStripMenuItem fileToolStripMenuItem = new ToolStripMenuItem() { Text = "File" };
+                MainMenuStrip.Items.Add(fileToolStripMenuItem);
+
+                foreach (var item in items)
+                    foreach (var subitem in item)
+                        fileToolStripMenuItem.DropDownItems.Add(new ToolStripMenuItem(subitem));
+
+                //fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripMenuItem[] { new ToolStripMenuItem("New") });
+            });
+        }
+
+        private void pnlHeader_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void btnMinMax_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+                WindowState = FormWindowState.Normal;
+            else
+                WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    public class CustomToolStripSeparator : ToolStripSeparator
+    {
+        public CustomToolStripSeparator() => Paint += CustomToolStripSeparator_Paint;
+
+        private void CustomToolStripSeparator_Paint(object sender, PaintEventArgs e)
+        {
+            // Get the separator's width and height.
+            ToolStripSeparator toolStripSeparator = (ToolStripSeparator)sender;
+            int width = toolStripSeparator.Width;
+            int height = toolStripSeparator.Height;
+            //Color foreColor = Color.Blue;
+            Color backColor = Color.FromArgb(255, 200, 200, 200);
+            Color foreColor = Color.FromArgb(255, 220, 220, 220);
+
+            // Fill the background.
+            e.Graphics.FillRectangle(new SolidBrush(backColor), 0, 0, width, height);
+            // Draw the line.
+            e.Graphics.DrawLine(new Pen(foreColor), 4, height / 2, width - 4, height / 2);
+        }
+    }
+
+    class MenuColorTable : ProfessionalColorTable
+    {
+        public MenuColorTable()
+        {
+            base.UseSystemColors = false;
+        }
+        public override Color MenuBorder
+        {
+            get { return Color.Fuchsia; }
+        }
+        public override Color MenuItemBorder
+        {
+            get { return Color.DarkViolet; }
+        }
+        public override Color MenuItemSelected
+        {
+            get { return Color.FromArgb(255, 200, 200, 200); }
+        }
+        public override Color MenuItemSelectedGradientBegin
+        {
+            get { return Color.FromArgb(255, 190, 190, 190); }
+        }
+        public override Color MenuItemSelectedGradientEnd
+        {
+            get { return Color.FromArgb(255, 180, 180, 180); }
+        }
+        public override Color MenuStripGradientBegin
+        {
+            get { return Color.FromArgb(255, 190, 190, 190); }
+        }
+        public override Color MenuStripGradientEnd
+        {
+            get { return Color.FromArgb(255, 180, 180, 180); }
         }
     }
 }
